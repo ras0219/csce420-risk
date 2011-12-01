d5061d3a9e9e56c5da2c07135e937f608e6310d0
Robert Schumacher <robertallenschumacher@gmail.com>
2011-11-30 18:00:17 -0600

    Added .gitignore
    Modified RiskError class to take an agent ID as a constructor argument.

--- riskerror.py
+++ riskerror.py
@@ -1,6 +1,6 @@
 class RiskError(Exception):
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, aid, value):
+        self.value = "[" + repr(aid) + "]" + str(value)
 
     def __str__(self):
         return repr(self.value)
