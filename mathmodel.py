ac727c0c7ddd25c1153c6cff2ab0cb530fb74cf4
Robert Schumacher <robertallenschumacher@gmail.com>
2011-11-30 18:33:44 -0600

    Further work towards implementing the simulation.
    100% - Pre-game Placement of pieces.
    030% - Attack moves + error handling
    000% - Transfer moves + error handling

--- mathmodel.py
+++ mathmodel.py
@@ -15,12 +15,18 @@ class MathModel:
     # perform_combat_round :: Integer -> Integer -> (Integer, Integer)
     def perform_combat_round(self, army1, army2):
         # Inputs:
-        #   army1 - size of player 1's army
-        #   army2 - size of player 2's army
+        #   army1 - size of player 1's army (Offense)
+        #   army2 - size of player 2's army (Defense)
         # Outputs:
         #   L1 - L1 is losses to player 1
         #   L2 - L2 is losses to player 2
         #
         # TODO: Implement chance
+        # Important note! It is ILLEGAL to return (army1,army2)
+        #   unless both arguments were 0!
+        if army1 == army2 and army1 == 0:
+            return (0,0)
         loss = min([army1, army2])
+        if army1 == army2:
+            return (loss, loss-1)
         return (loss, loss)
--- simulation.py
+++ simulation.py
@@ -3,7 +3,6 @@ import random
 
 class Simulation:
     PHASE_PREGAME = 0
-    PHASE_PREPLACE = 1
     PHASE_PLACE = 2
     PHASE_ATTACK = 3
     PHASE_TRANSFER = 4
@@ -11,7 +10,6 @@ class Simulation:
     # data Simulation = Simulation Edgelist MathModel
     def __init__(self, elist, mmodel):
         self.edgelist = elist
-
         self.model = mmodel
         self.agent_id = 0
         self.phase = PHASE_PREGAME
@@ -20,41 +18,62 @@ class Simulation:
     # add_agent :: Agent -> IO ()
     def add_agent(self, agent):
         agent.set_id(agent_id)
-        agents[agent_id] = agent
-        agent_id++ # Maybe change this to have agents be colors?
+        self.agents[agent_id] = agent
+        # Maybe change this to have agents be colors?
+        agent_id += 1
 
     # start :: IO ()
     def start(self):
-        while not is_ended():
-            if phase == PHASE_PREGAME:
-                # initialize map
-                aids = agents.keys()
-                num_agents = len(aids)
-                self.countries = {}
-                self.owns = {}
-                clist = self.edgelist.keys()
-                random.shuffle(clist)
-                for k in agents:
-                    self.owns[k] = []
-                for c in range(len(clist)):
-                    aid = aids[c % num_agents]
-                    self.owns[aid].append(clist[c])
-                    self.countries[clist[c]] = aid
-                self.phase = PHASE_PREPLACE
-            elif phase == PHASE_PREPLACE:
-                numarmies = self.model.pregame_armies
-                for aid in agents:
-                    places = agents[aid].pregame_place(numarmies, self)
-                    if sum(places.values()) > numarmies:
-                        raise RiskError(str(aid)+" placed too many armies")
-                    for p in places:
-                        if 
+        # initialize map
+        aids = self.agents.keys()
+        num_agents = len(aids)
+        self.countries = {}
+        self.owns = {}
+        self.armies = {}
+        clist = self.edgelist.keys()
+        random.shuffle(clist)
+        for k in self.agents:
+            self.owns[k] = []
+        for c in range(len(clist)):
+            aid = aids[c % num_agents]
+            self.owns[aid].append(clist[c])
+            self.countries[clist[c]] = aid
+            self.armies[clist[c]] = 1
 
-            for aid in agents:
-                eval_turn(aid)
+            numarmies = self.model.pregame_armies
+            for aid in self.agents:
+                places = self.agents[aid].pregame_place(numarmies, self)
+                if sum(places.values()) > numarmies:
+                    raise RiskError(aid, "Too many pregame armies")
+                for p in places:
+                    if p not in countries:
+                        raise RiskError(aid, "Invalid Country "+repr(p))
+                    if not countries[p] == aid:
+                        raise RiskError(aid, "Unowned Country "+repr(p))
+                    if places[p] < 0:
+                        raise RiskError(aid, "Negative Armies")
+                    self.armies[p] += places[p]
 
+        # Begin game
+        for aid in self.agents:
+            for mv in generate(aid.attack):
+                if mv[0] not in owns[aid]:
+                    raise RiskError(aid, "Unowned Source Country "+repr(mv[0]))
+                if mv[1] in owns[aid]:
+                    raise RiskError(aid, "Owned Dest Country "+repr(mv[1]))
+                if mv[2]+1 > armies[mv[0]]:
+                    raise RiskError(aid, "Too many Armies")
+                if mv[2] < 1:
+                    raise RiskError(aid, "Not Enough Armies")
+
+                self.model.perform_combat_round(
     # eval_turn :: Agent -> IO ()
-    def eval_turn(self, aid):
+    def generate(self, agent_func):
+        while True:
+            mv = agent_func(self)
+            if mv == None:
+                break
+            yield mv
 
     # is_ended :: Bool
     def is_ended(self):
