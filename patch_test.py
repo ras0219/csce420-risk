f9c5ab28845843ac53f9e27fab42ff3d948e898b
Robert Schumacher <robertallenschumacher@gmail.com>
2011-12-01 16:35:20 -0600

    Improved performance via sparse matricies

--- patch_test.py
+++ patch_test.py
@@ -1,20 +1,20 @@
 #from scipy.stsci import convolve
 import numpy as np
 
-rounds = 120
-patch1 = [[0, 0, 0.30],
-         [0, 0.50, 0],
-         [0.20, 0, 0]]
+rounds = 450
+patch1 = {(0,2):0.30,
+          (1,1):0.50,
+          (2,0):0.20}
 
-patch2 = [[0, 0.50],
-         [0.50, 0]]
+patch2 = {(0,1):0.50,
+          (1,0):0.50}
 
-patch3 = [[1]]
+patch3 = {(0,0):1.00}
 
-curpatch = [[1]] # np.zeros((rounds*2+1, rounds*2+1))
+curpatch = patch3.copy() # np.zeros((rounds*2+1, rounds*2+1))
 
-armies1 = 50
-armies2 = 20
+armies1 = 300
+armies2 = 300
 
 # curpatch[0][0] = 1
 
@@ -24,30 +24,28 @@ armies2 = 20
 
 #print out
 
-for x in range(rounds):
-    size = len(patch1)+len(curpatch)-1
-    newpatch = [[0 for x in range(size)] for y in range(size)]
-    for r in range(len(curpatch)):
-        for c in range(len(curpatch[r])):
-            patch = patch1
-            if armies1-r <= 0 or armies2-c <= 0:
-                patch = patch3
-            elif armies1-r == 1 or armies2-c == 1:
-                patch = patch2
+def add(t1, t2):
+    return (t1[0]+t2[0], t1[1]+t2[1])
 
-            for r2 in range(len(patch)):
-                for c2 in range(len(patch[r2])):
-                    newpatch[r+r2][c+c2] += patch[r2][c2] * curpatch[r][c]
+for x in range(rounds):
+    newpatch = {}
+    for k,v in curpatch.items():
+        patch = patch1
+        if armies1-k[0] <= 0 or armies2-k[1] <= 0:
+            patch = patch3
+        elif armies1-k[0] == 1 or armies2-k[1] == 1:
+            patch = patch2
+        for k2,v2 in patch.items():
+            nk = add(k,k2)
+            if v > 0 and v2 > 0:
+                newpatch[nk] = newpatch.get(nk, 0.00) + v*v2
     curpatch = newpatch
 
-for r in range(min([len(curpatch),armies1+1])):
-    for c in range(min([len(curpatch[r]),armies2+1])):
-        print "%#02.2f " % (curpatch[r][c]*100),
-    print ""
+for k in curpatch:
+    print "%-10s %#02.8f " % (k, curpatch[k]*100)
 
 acc = 0
 for r in curpatch:
-    for c in r:
-       acc += c
+    acc += curpatch[r]
 
 print acc
