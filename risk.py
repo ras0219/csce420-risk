e0dcbbacb4a92a7ee0c9d1a8be55ededbeb00b4c
Robert Schumacher <robertallenschumacher@gmail.com>
2011-12-05 02:47:21 -0600

    Minor improvement in statistics reporting

--- risk.py
+++ risk.py
@@ -7,15 +7,15 @@ def main():
     model = mathmodel.MathModel()
     elist = riskboard.territory_adjacency
     winners = {}
-    for n in range(100):
+    for n in range(50):
         sim = simulation.Simulation(elist, model)
-        sim.add_agent(test_agent.TestAgent())
-        sim.add_agent(test_agent.TestAgent())
-        sim.add_agent(test_agent.TestAgent())
-        sim.add_agent(test_agent.TestAgent())
+        sim.add_agent(test_agent.TestAgent(0))
+        sim.add_agent(test_agent.TestAgent(0))
+        sim.add_agent(test_agent.TestAgent(1))
+        sim.add_agent(test_agent.TestAgent(1))
         sim.start()
         winner = sim.winner()
-        print "Winner is %s" % winner
+        print "%3d) Winner is %s" % (n, winner)
         winners[str(winner)] = winners.get(str(winner), 0) + 1
     print winners
         
