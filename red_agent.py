d1a2a5e7226a2d6ef79289744eaf3d45e2b79780
Robert Schumacher <robertallenschumacher@gmail.com>
2011-12-05 07:32:24 -0600

    Removed red_agent.py from master

--- red_agent.py
+++
@@ -1,7 +0,0 @@
-import agent
-from mathmodel import integral2d
-
-class RedAgent(agent.Agent):
-    def pregame_place(self, numarmies, sim):
-        c = random.choice(sim.owns[self])
-        return {c:numarmies}
