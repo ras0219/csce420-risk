5d9f2fd0c6e54268a067795e0b8f750a1bf05329
Robert Schumacher <robertallenschumacher@gmail.com>
2011-12-06 02:52:17 -0600

    Integrated transport code from RedAgent

--- secret_agent.py
+++ secret_agent.py
@@ -6,6 +6,7 @@ import graph_funcs
 class SecretAgent(agent.Agent):
 	def __init__(self):
 		self.mm = mathmodel.MathModel()
+		self.terraset = set()
 
 	def preferred_ids(self, num):
 		return ["Secret Agent Man #%02d" % num]
@@ -49,14 +50,14 @@ class SecretAgent(agent.Agent):
 				# LOL there's a function for that LOL
 				score = self.mm.chance_to_win(sim.armies[country], sim.armies[neighbor])
 				scorez += [(country, neighbor, score)]
-#			print country,neighbors
-#			print scorez
+			#print country,neighbors
+			#print scorez
 			#raw_input()
 
 		if len(scorez) > 0:
 			victim = sorted(scorez, key = lambda i: i[2]).pop()
 			return (victim[0], victim[1], sim.armies[victim[0]] - 1)
-#		print "Done"
+		#print "Done"
 		#raw_input()
 		return None
 
@@ -74,6 +75,48 @@ class SecretAgent(agent.Agent):
 		coolest = max(threatened, key=lambda i: i[1])
 
 		return {coolest[0]: numarmies}
+
+	# Original Source: RedAgent
+	def transfer(self, sim):
+		owned = set(sim.owns[self])
+		if self.terraset != owned:
+			if sim.is_ended():
+				return None
+			# update radius graph
+			self.tree = {}
+
+			def on_current_edge(c):
+				for k in sim.edgelist[c]:
+					if sim.countries[k] != self:
+						return True
+					if k in self.tree:
+						return True
+				return False
+
+			dist_to_edge = 0
+			while len(owned) > 0:
+				edges = filter(on_current_edge, owned)
+				for e in edges:
+					self.tree[e] = dist_to_edge
+					owned.remove(e)
+				dist_to_edge += 1
+			self.terraset = set(sim.owns[self])
+			self.unactivated = self.terraset.copy()
+
+		owned = sorted(self.unactivated, key=lambda c: self.tree[c], reverse=True)
+		while len(owned) > 0:
+			c = owned.pop()
+			if self.tree[c] == 0:
+				continue
+			if sim.armies[c] > 1:
+				# find the gradient
+				for k in sim.edgelist[c]:
+					if k in self.unactivated:
+						if self.tree[k] < self.tree[c]:
+							self.unactivated.remove(k)
+							return (c, k, sim.armies[c]-1)
+		self.unactivated = self.terraset.copy()
+		return None
 
 	def army_size(self,c):
 		return self.sim.armies[c]
